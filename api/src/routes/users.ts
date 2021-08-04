915e6063add70492fbaab417ce8fdf2705e04d21
logandarby <logandarby4@gmail.com>
2021-08-04 09:09:43 -0400

    refactor user registration to `/user` route

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,5 +1,17 @@
 import { Router } from "express";
 
+import User from "../models/User";
+import { encryptPassword } from "../util/passwordUtils";
+
 const users = Router();
+
+users.route("/").post(async (req, res) => {
+  const encrypedPassword = await encryptPassword(req.body.password);
+  await User.create({
+    username: req.body.username,
+    password: encrypedPassword,
+  });
+  res.redirect("/login");
+});
 
 export default users;
