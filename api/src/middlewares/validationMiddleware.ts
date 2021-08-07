81d277230a85f84d1e9ba38f415d70657059c4ab
logandarby <logandarby4@gmail.com>
2021-08-07 12:23:09 -0400

    Add validation to user route

--- api/src/middlewares/validationMiddleware.ts
+++ api/src/middlewares/validationMiddleware.ts
@@ -1,16 +1,18 @@
 import { Response, Request, NextFunction } from "express";
-import { validationResult } from "express-validator";
+import { body, validationResult } from "express-validator";
+
+type ExpressMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Response | void;
 
 /**
  * A middleware that validates any express-validator validations/sanitizers.
  * Returns code 400 with the errors array if the request is invalid.
  */
 
-export const validateRequest = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const validateRequest: ExpressMiddleware = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -18,3 +20,15 @@ export const validateRequest = (
     next();
   }
 };
+
+// Below are the functions used to wrap and standardize express-validator
+// validations/sanitizers
+
+export const checkEmail = body("email")
+  .trim()
+  .normalizeEmail({ all_lowercase: false })
+  .isEmail();
+
+export const checkPassword = body("password")
+  .trim()
+  .isLength({ min: 6, max: 30 });
--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -2,16 +2,23 @@ import { Router } from "express";
 
 import User from "../models/User";
 import { encryptPassword } from "../util/passwordUtils";
+import {
+  checkEmail,
+  checkPassword,
+  validateRequest,
+} from "../middlewares/validationMiddleware";
 
 const users = Router();
 
-users.route("/").post(async (req, res) => {
-  const encrypedPassword = await encryptPassword(req.body.password);
-  await User.create({
-    email: req.body.email,
-    password: encrypedPassword,
+users
+  .route("/")
+  .post(checkEmail, checkPassword, validateRequest, async (req, res) => {
+    const encrypedPassword = await encryptPassword(req.body.password);
+    await User.create({
+      email: req.body.email,
+      password: encrypedPassword,
+    });
+    res.redirect("/login");
   });
-  res.redirect("/login");
-});
 
 export default users;
