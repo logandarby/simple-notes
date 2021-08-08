9dc3f4a4597a2b129f79c28bb59862d5b5716275
logandarby <logandarby4@gmail.com>
2021-08-08 07:40:36 -0400

    create basic postgres server setup with  session store

---
+++ api/src/entity/Session.ts
@@ -0,0 +1,15 @@
+import { ISession } from "connect-typeorm";
+import { BaseEntity, Column, Entity, Index, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Session extends BaseEntity implements ISession {
+  @PrimaryColumn("varchar", { length: 255 })
+  public id = "";
+
+  @Index()
+  @Column("bigint")
+  public expiredAt = Date.now();
+
+  @Column("text")
+  public json = "";
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,31 @@
+import "reflect-metadata";
 import express from "express";
 import session from "express-session";
 import passport from "passport";
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
+import { TypeormStore } from "connect-typeorm";
 
 import "./config/passport";
-import connectDatabase from "./config/database";
 import routes from "./routes";
 import { isLoggedIn } from "./middlewares/authMiddleware";
+import { Session } from "./entity/Session";
+import { createConnection } from "typeorm";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 const SECRET = process.env.SECRET ?? "Ey2pTsjgZ26NKXv4GsJlXuCO4pSEM8g9";
 
 const main = async () => {
-  let sessionStore;
-  ({ sessionStore } = await connectDatabase());
+  // let sessionStore;
+  // ({ sessionStore } = await connectDatabase());
+
+  const connection = await createConnection();
+  console.log("Database Connected 💾");
+  const sessionRepository = connection.getRepository(Session);
+  const sessionStore = new TypeormStore({
+    cleanupLimit: 2,
+  }).connect(sessionRepository);
 
   // Initializing middleware
   app.use(express.json());
