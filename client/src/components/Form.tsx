6ad0ad054e38f902ba9ea0ee4e927e631d8aa79c
logandarby <logandarby4@gmail.com>
2021-08-24 08:39:25 -0400

    Redo form and add onSubmit prop

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,21 +1,68 @@
+import React, { useState } from "react";
+
 import "./Form.scss";
 import Button from "./Button";
 import Input from "./Input";
-import React from "react";
 
-interface FormProps extends React.HTMLProps<HTMLFormElement> {}
+export interface FormBody {
+  email: string;
+  password: string;
+}
 
-function Form({ className, ...rest }: FormProps) {
+export type FormSubmitFunction = (body: FormBody) => void;
+
+export interface FormProps {
+  onSubmit: FormSubmitFunction;
+  className?: string;
+}
+
+function Form({ className, onSubmit }: FormProps) {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handlePasswordChange: React.FormEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const handleEmailChange: React.FormEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    onSubmit({ email, password });
+  };
+
   return (
-    <form className={`Form ${className}`} {...rest}>
+    <form
+      className={`Form ${className}`}
+      autoComplete="off"
+      noValidate
+      onSubmit={handleSubmit}
+    >
       <label htmlFor="email" className="Form__Label">
         email
       </label>
-      <Input name="email" className="Form__Input" />
+      <Input
+        name="email"
+        className="Form__Input"
+        value={email}
+        onChange={handleEmailChange}
+      />
       <label htmlFor="password" className="Form__Label Form__Label--password">
         password
       </label>
-      <Input type="password" name="password" className="Form__Input" />
+      <Input
+        type="password"
+        name="password"
+        className="Form__Input"
+        value={password}
+        onChange={handlePasswordChange}
+      />
       <Button className="Form__Button" text="sumbit" />
     </form>
   );
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,22 @@
 import "./Login.scss";
-import Form from "../components/Form";
+import Form, { FormBody } from "../components/Form";
 
 function Login() {
+  const handleSubmit = (body: FormBody) => {
+    fetch("http://localhost:4000/session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((res) => {
+      console.log(res.json());
+    });
+  };
+
   return (
     <div className="Login">
-      <Form />
+      <Form onSubmit={handleSubmit} />
     </div>
   );
 }
