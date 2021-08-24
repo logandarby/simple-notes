993830e7d500cece648a96313e48e39a9590eb40
logandarby <logandarby4@gmail.com>
2021-08-24 15:08:51 -0400

    Style notes

--- client/src/pages/Notes.tsx
+++ client/src/pages/Notes.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { Note } from "../apiResources";
+
 import "./Notes.scss";
+import NoteContainer from "./notes/NoteContainer";
 
 function Notes() {
+  const [notes, setNotes] = useState([]);
+
+  // Fetch notes on component mount
+  useEffect(() => {
+    fetch("http://localhost:4000/notes", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setNotes(json);
+      });
+  }, []);
+
   return (
     <div className="Notes">
-      <main>notes</main>
+      <main>
+        {notes.map((note: Note) => {
+          return <NoteContainer note={note} key={note.id} />;
+        })}
+      </main>
     </div>
   );
 }
---
+++ client/src/pages/notes/NoteContainer.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import "./NoteContainer.scss";
+import { Note } from "../../apiResources";
+
+interface NoteContainerProps {
+  note: Note;
+}
+
+function NoteContainer(props: NoteContainerProps) {
+  return (
+    <div className="Note">
+      <h1 className="Note__Title">{props.note.title}</h1>
+      <div className="Note__Contents">{props.note.contents}</div>
+    </div>
+  );
+}
+
+export default NoteContainer;
