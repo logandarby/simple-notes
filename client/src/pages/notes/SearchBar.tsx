1f26e372385bd7e44889a1b38db75f874e929ca3
logandarby <logandarby4@gmail.com>
2021-08-26 10:06:40 -0400

    Fix console warning

--- client/src/pages/notes/SearchBar.tsx
+++ client/src/pages/notes/SearchBar.tsx
@@ -6,14 +6,14 @@ export interface SearchBarProps extends InputProps {
   setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
 }
 
-function SearchBar(props: SearchBarProps) {
+function SearchBar({ searchQuery, setSearchQuery, ...props }: SearchBarProps) {
   return (
     <div className={`SearchBar ${props.className}`}>
       <img className="SearchBar__Icon" src="searchIcon.svg" alt="" />
       <Input
         {...props}
-        value={props.searchQuery}
-        onChange={(e) => props.setSearchQuery(e.currentTarget.value)}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.currentTarget.value)}
         placeholder="search for notes"
         className={`SearchBar__Input`}
       />
